14b44fc1d52d850477221803928da5ae705224d1
MasterGameFTW3561 <mastergame3561@gmail.com>
2017-10-15 11:34:44 +0800

    heh

--- Calamity/files.js
+++ Calamity/files.js
@@ -1,5 +1,6 @@
 {
 	"patch.js": 2056392651,
 	"th11/background/stage01/stage01a.png": 60659947,
-	"th11/background/stage01/stage01b.png": 785473982
+	"th11/background/stage01/stage01b.png": 785473982,
+	"th11/enemy/enemy.png": 3309331552
 }
