f87f6ee31861ba91eba5035a3dede3aebd145883
MasterGameFTW3561 <mastergame3561@gmail.com>
2017-10-15 15:49:47 +0800

    feels keke man

--- Calamity/files.js
+++ Calamity/files.js
@@ -1,6 +1,6 @@
 {
 	"patch.js": 2056392651,
-	"th11/background/stage01/stage01a.png": 60659947,
+	"th11/background/stage01/stage01a.png": 424004607,
 	"th11/background/stage01/stage01b.png": 785473982,
 	"th11/card/cdbg01a.png": 2970789869,
 	"th11/card/cdbg01b.png": 3390744468,
@@ -8,9 +8,9 @@
 	"th11/face/enemy1/ename01.png": 2469685979,
 	"th11/face/enemy1/face01ct.png": 189292220,
 	"th11/face/enemy1/face01dp.png": 365244305,
-	"th11/face/enemy1/face01no.png": 619522547,
+	"th11/face/enemy1/face01no.png": 4230745712,
 	"th11/face/enemy1/face01sp.png": 3237156295,
 	"th11/face/enemy1m/face01mct.png": 1238298197,
-	"th11/front/ename.png": 497777710,
+	"th11/front/ename.png": 1277033088,
 	"th11/stgenm/stg1enm.png": 2295244035
 }
