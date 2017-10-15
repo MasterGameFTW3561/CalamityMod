fe63746f123c2f56ee351e7162ba8185a2328632
MasterGameFTW3561 <mastergame3561@gmail.com>
2017-10-15 11:20:52 +0800

    fix

--- Calamity/files.js
+++ Calamity/files.js
@@ -1,5 +1,5 @@
 {
 	"patch.js": 2056392651,
-	"th11/background/stage01/stage01a.png": 485342242,
+	"th11/background/stage01/stage01a.png": 60659947,
 	"th11/background/stage01/stage01b.png": 785473982
 }
