5ab42b2e013e35095c468328851f925c4a0bae61
MasterGameFTW3561 <mastergame3561@gmail.com>
2017-10-15 15:22:51 +0800

    haha

--- Calamity/files.js
+++ Calamity/files.js
@@ -2,5 +2,13 @@
 	"patch.js": 2056392651,
 	"th11/background/stage01/stage01a.png": 60659947,
 	"th11/background/stage01/stage01b.png": 785473982,
-	"th11/enemy/enemy.png": 3309331552
+	"th11/enemy/enemy.png": 3309331552,
+	"th11/face/enemy1/ename01.png": 2469685979,
+	"th11/face/enemy1/face01ct.png": 2628363972,
+	"th11/face/enemy1/face01dp.png": 365244305,
+	"th11/face/enemy1/face01no.png": 619522547,
+	"th11/face/enemy1/face01sp.png": 3237156295,
+	"th11/face/enemy1m/face01mct.png": 1238298197,
+	"th11/front/ename.png": 497777710,
+	"th11/stgenm/stg1enm.png": 2295244035
 }
